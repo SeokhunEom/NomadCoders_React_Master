8a55ba72e5df2dc0395197732d425600b5f13b61
SeokHun <tommya98@naver.com>
2024-09-23 05:46:31 +0000

    Trello 드래그드롭 초기 구현

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -1,36 +1,64 @@
-import { DragDropContext, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
-import { StrictModeDroppable } from "./StrictModeDroppable";
+import DraggableCard from "./components/DraggableCard";
+import { StrictModeDroppable as Droppable } from "./StrictModeDroppable";
+import styled from "styled-components";
+import { toDoState } from "./atoms";
+import { useRecoilState } from "recoil";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
 
 function App() {
-  const onDragEnd = () => {};
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    if (!destination) return;
+
+    setToDos((oldToDos) => {
+      const copyToDos = [...oldToDos];
+      copyToDos.splice(source.index, 1);
+      copyToDos.splice(destination.index, 0, draggableId);
+      return copyToDos;
+    });
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
-        <StrictModeDroppable droppableId="one">
-          {(magic) => (
-            <ul ref={magic.innerRef} {...magic.droppableProps}>
-              <Draggable draggableId="first" index={0}>
-                {(magic) => (
-                  <li ref={magic.innerRef} {...magic.draggableProps}>
-                    <span {...magic.dragHandleProps}>⭐️</span>
-                    One
-                  </li>
-                )}
-              </Draggable>
-              <Draggable draggableId="second" index={1}>
-                {(magic) => (
-                  <li ref={magic.innerRef} {...magic.draggableProps}>
-                    <span {...magic.dragHandleProps}>⭐️</span>
-                    Two
-                  </li>
-                )}
-              </Draggable>
-            </ul>
-          )}
-        </StrictModeDroppable>
-      </div>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((toDo, index) => (
+                  <DraggableCard key={toDo} toDo={toDo} index={index} />
+                ))}
+                {magic.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   );
 }
--- trello/src/main.tsx
+++ trello/src/main.tsx
@@ -1,9 +1,18 @@
 import App from "./App.tsx";
+import GlobalStyle from "./GlobalStyle.tsx";
+import { RecoilRoot } from "recoil";
 import { StrictMode } from "react";
+import { ThemeProvider } from "styled-components";
 import { createRoot } from "react-dom/client";
+import { darkTheme } from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <RecoilRoot>
+        <GlobalStyle />
+        <App />
+      </RecoilRoot>
+    </ThemeProvider>
   </StrictMode>
 );
