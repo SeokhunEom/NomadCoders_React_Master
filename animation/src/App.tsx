04ae72e4e29468baf1946c6b269d301d37ae4625
SeokHun <tommya98@naver.com>
2024-10-10 23:09:43 +0900

    애니메이션연습 gestures

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
-import { s } from "framer-motion/client";
 import styled from "styled-components";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -10,63 +10,59 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
+  hover: {
+    scale: 1.5,
+    rotateZ: 90,
   },
-  end: {
+  click: {
     scale: 1,
-    opacity: 1,
+    borderRadius: "100px",
+  },
+  drag: {
+    backgroundColor: "rgb(46, 204, 113)",
     transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
+      duration: 10,
     },
   },
 };
 
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
-};
-
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileDrag="drag"
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
