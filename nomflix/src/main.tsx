8aba088604e1b5c83ab4a6944dd21b22f02e8f4e
SeokHun <tommya98@naver.com>
2024-10-17 03:41:41 +0000

    놈플릭스 홈

--- nomflix/src/main.tsx
+++ nomflix/src/main.tsx
@@ -1,18 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./App.tsx";
 import GlobalStyle from "./GlobalStyle.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
 import { StrictMode } from "react";
 import { ThemeProvider } from "styled-components";
 import { createRoot } from "react-dom/client";
 import { theme } from "./theme.ts";
 
+const client = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </RecoilRoot>
   </StrictMode>
 );
