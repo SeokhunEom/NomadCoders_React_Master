ef47e94c574372fb0ddd54b76949c5924292c3e5
SeokHun <tommya98@naver.com>
2024-10-28 12:14:32 +0900

    gatsby mdx 플러그인 추가

---
+++ dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout";
+import React from "react";
+
+function BlogPost() {
+  return (
+    <Layout title="Blog Post">
+      <div>hi</div>
+    </Layout>
+  );
+}
+
+export default BlogPost;
