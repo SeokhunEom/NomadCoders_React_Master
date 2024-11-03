39a56a15987619d29ef69062de546bbe6fff2349
SeokHun <tommya98@naver.com>
2024-11-03 12:27:08 +0900

    gatsby 블로그포스트 구현

--- dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ dev-stickers/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,12 +1,38 @@
 import Layout from "../../components/Layout";
 import React from "react";
+import Seo from "../../components/Seo";
+import { graphql } from "gatsby";
 
-function BlogPost() {
+interface BlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: React.ReactNode;
+}
+
+function BlogPost({ data, children }: BlogPostProps) {
   return (
     <Layout title="Blog Post">
-      <div>hi</div>
+      <div>{children}</div>
     </Layout>
   );
 }
 
 export default BlogPost;
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        title
+        author
+        date
+        category
+        slug
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: BlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
